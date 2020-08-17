c214c31918353489b13478d89c640212eae75f5d
lmtruong1512 <lemanhtruong.ptit@gmail.com>
2020-08-17 20:38:17 +0700

    Add: 17-8-2020 report

--- VietIS/app.js
+++ VietIS/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var bodyParser = require('body-parser')
 const app = express();
-const port = 33555;
+const port = process.env.PORT || 15128;
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
